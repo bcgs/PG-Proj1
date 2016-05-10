1aa16e324c8099c85faedd0dc90e406b8928095f
bcgs <bruno.bcgs@gmail.com>
2016-05-10 01:46:44 -0300

    Basic update
    - Refactoring
    - New methods and functions added.

--- source/main.cpp
+++ source/main.cpp
@@ -9,44 +9,131 @@ const int WINDOW_W = 500;
 const int WINDOW_H = 500;
 
 std::vector <Ponto> pontos;
+std::vector <Ponto> primeiraDerivada;;
+std::vector <Ponto> segundaDerivada;
 bool bezier = false;
 
-void display() {
-    glClear(GL_COLOR_BUFFER_BIT);
-    
-    if(pontos.size() > 0) {
-        
-        glBegin(GL_LINE_STRIP);
-        glColor3f(1.0f, 1.0f, 1.0f);
-        if (pontos.size() > 1) {
-            if (bezier) {
-                Ponto p(0,0);
-                for (float t = 0.0; t <= 1.0; t += 0.01) {
-                    MathUtil::bezier(pontos, t, p);
-                    glVertex2d(p.x, p.y);
-                }
-            } else {
-                for (auto p : pontos)
-                    glVertex2d(p.x, p.y);
-            }
-        }
-        //else {
-            //auto p = pontos.front();
-            //glVertex2d(p.x, p.y);
-        //}
-        glEnd();
-        
-        glPointSize(5.0f);
-        glBegin(GL_POINTS);
-        glColor3f(1.0f, 0.0f, 0.0f);
-        for(auto p : pontos)
-            glVertex2d(p.x, p.y);
-        glEnd();
-    }
-    glFlush();
+void drawPoints();
+void drawLines(const std::vector<Ponto>& pontos);
+void drawBezierCurve(const std::vector<Ponto>& pontos);
+void orthogonalization(Ponto u, Ponto v, Ponto& w);
+std::vector<Ponto> getDerivativeControllers(const std::vector<Ponto> u);
+
+void display()
+{
+	glClear(GL_COLOR_BUFFER_BIT);
+	drawPoints();
+
+	pontos.push_back(Ponto(100.0f,5.0f));
+	pontos.push_back(Ponto(200.0f,150.0f));
+	pontos.push_back(Ponto(10.0f,300.0f));
+	pontos.push_back(Ponto(190.0f,125.0f));
+
+	drawLines(pontos);
+
+	primeiraDerivada = getDerivativeControllers(pontos);
+	segundaDerivada = getDerivativeControllers(primeiraDerivada);
+
+	Ponto p(0,0);
+	MathUtil::bezier(pontos, 0.5f, p);
+	std::cout << "(" << p.x << "," << p.y << ")\n";
+
+	Ponto u(0,0);
+	MathUtil::bezier(primeiraDerivada, 0.5f, u);
+	std::cout << "pd(" << u.x << "," << u.y << ")\n";
+
+	Ponto v(0,0);
+	MathUtil::bezier(segundaDerivada, 0.5f, v);
+	std::cout << "sd(" << v.x << "," << v.y << ")\n";
+
+	Ponto w(0,0);
+	orthogonalization(u,v,w);
+	std::cout << "ortho(" << w.x << "," << w.y << ")\n";
+
+
+	if(pontos.size() > 1) {
+
+		drawLines(pontos);
+		if(bezier) drawBezierCurve(pontos);
+/*
+		primeiraDerivada = getDerivativeControllers(pontos);
+		segundaDerivada = getDerivativeControllers(primeiraDerivada);
+
+		for(auto p : pontos)
+			std::cout << "(" << p.x << "," << p.y << ")\n";
+
+		Ponto p(0,0);
+		MathUtil::bezier(pontos, 0.5f, p);
+		std::cout << "bz(" << p.x << "," << p.y << ")\n";
+
+		Ponto u(0,0);
+		MathUtil::bezier(primeiraDerivada, 0.5f, u);
+		std::cout << "pd(" << u.x << "," << u.y << ")\n";
+
+		Ponto v(0,0);
+		MathUtil::bezier(segundaDerivada, 0.5f, v);
+		std::cout << "sd(" << v.x << "," << v.y << ")\n";
+
+		Ponto w(0,0);
+		orthogonalization(u,v,w);
+		std::cout << "ortho(" << w.x << "," << w.y << ")\n";
+*/
+	}
+	glFlush();
 }
 
-void reshape(int w, int h) {
+void drawPoints()
+{
+	glPointSize(5.0f);
+	glBegin(GL_POINTS);
+	glColor3f(1.0f, 0.0f, 0.0f);
+	for(auto p : pontos)
+		glVertex2d(p.x, p.y);
+	glEnd();
+}
+
+void drawLines(const std::vector<Ponto>& pontos)
+{
+	glBegin(GL_LINE_STRIP);
+	glColor3f(0.0f, 1.0f, 1.0f);
+	for(auto p : pontos)
+		glVertex2d(p.x, p.y);
+	glEnd();
+}
+
+void drawBezierCurve(const std::vector<Ponto>& pontos)
+{
+	glBegin(GL_LINE_STRIP);
+	glColor3f(1.0f, 1.0f, 0.0f);
+	Ponto p(0,0);
+	for (float t = 0.0f; t < 1.0f; t += 0.01f) {
+		MathUtil::bezier(pontos, t, p);
+		glVertex2d(p.x,p.y);
+	}
+	glEnd();
+}
+
+std::vector<Ponto> getDerivativeControllers(const std::vector<Ponto> u)
+{
+	std::vector<Ponto> v;
+	for (int i = 0; i < u.size()-1; ++i)
+		v.push_back(Ponto(u[i+1].x - u[i].x, u[i+1].y - u[i].y));
+	return v;
+}
+
+float scalarProduct(Ponto u, Ponto v)
+{
+	return (u.x * v.x + u.y * v.y);
+}
+
+void orthogonalization(Ponto u, Ponto v, Ponto& w)
+{
+	w.x = v.x - (scalarProduct(u,v)/scalarProduct(u,u)) * u.x;
+	w.y = v.y - (scalarProduct(u,v)/scalarProduct(u,u)) * u.y;
+}
+
+void reshape(int w, int h)
+{
     glViewport(0, 0, (GLsizei) w, (GLsizei) h);
     
     glMatrixMode(GL_PROJECTION);
@@ -56,27 +143,32 @@ void reshape(int w, int h) {
     glLoadIdentity();
 }
 
-void handleKeypress(unsigned char key, int x, int y) {
+void handleKeypress(unsigned char key, int x, int y)
+{
     switch (key) {
         case 27: // ESC
             exit(0);
         case 'b':
             bezier = !bezier;
+            break;
     }
     glutPostRedisplay();
 }
 
-void handleMouseClick(int button, int state, int x, int y) {
+void handleMouseClick(int button, int state, int x, int y)
+{
     if (button == GLUT_LEFT_BUTTON)
-        if (state == GLUT_DOWN)
+        if (state == GLUT_DOWN) {
             pontos.push_back(Ponto(x, y));
-    
+            std::cout << "(" << x << "," << y << ")\n";
+        }
     glutPostRedisplay();
 }
 
-int main(int argc, char ** argv) {
+int main(int argc, char ** argv)
+{
     glutInit(&argc, argv);
-    glutInitWindowPosition(0, 0); // upper left
+    glutInitWindowPosition(0,0); // upper left
     glutInitWindowSize(WINDOW_W, WINDOW_H);
     glutInitDisplayMode(GLUT_RGB | GLUT_SINGLE);
     
--- source/mathutil.h
+++ source/mathutil.h
@@ -7,7 +7,8 @@
 
 namespace MathUtil {
 
-    unsigned long long factorial(int n) {
+    unsigned long long factorial(int n)
+    {
         long long fat;
         
         if (n <= 1) {
@@ -22,22 +23,38 @@ namespace MathUtil {
         return fat;
     }
     
-    unsigned long long comb(int n, int i) {
+    signed long long comb(int n, int i)
+    {
         unsigned long long ret = factorial(n)/(factorial(i) * factorial(n - i));
         return ret;
     }
     
-    inline void bezier(const std::vector<Ponto>& pontos, float t, Ponto& pout) {
+    inline void bezier(const std::vector<Ponto>& pontos, float t, Ponto& pout)
+    {
         float x = 0.0;
         float y = 0.0;
-        const unsigned int vertices = (unsigned int)pontos.size();
+        const unsigned int n = (unsigned int)pontos.size()-1;
         
-        for (int i = 0; i < vertices; i++) {
-            x += comb(vertices - 1, i) * pow((1 - t), vertices-1-i) * pow(t, i) * pontos[i].x;
-            y += comb(vertices - 1, i) * pow((1 - t), vertices-1-i) * pow(t, i) * pontos[i].y;
+        for (int i = 0; i < pontos.size(); i++) {
+            x += comb(n,i) * pow((1-t), n-i) * pow(t,i) * pontos[i].x;
+            y += comb(n,i) * pow((1-t), n-i) * pow(t,i) * pontos[i].y;
         }
         pout.x = x;
         pout.y = y;
+    }
+
+    inline void derivative(const std::vector<Ponto>& pontos, float t, Ponto& pout)
+    {
+    	float x = 0.0;
+		float y = 0.0;
+		const unsigned int n = (unsigned int)pontos.size()-1;
+
+		for (int i = 0; i < pontos.size(); i++) {
+			x += (-comb(n,i) * (n-i) * pow((1-t), n-i-1) * pow(t,i) + comb(n,i) * i * pow((1-t),n-i) * pow(t,i-1)) * pontos[i].x;
+			y += (-comb(n,i) * (n-i) * pow((1-t), n-i-1) * pow(t,i) + comb(n,i) * i * pow((1-t),n-i) * pow(t,i-1)) * pontos[i].y;
+		}
+		pout.x = x;
+		pout.y = y;
     }
 }
 
